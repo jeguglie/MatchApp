2c125023ccd3c19018e2dc1108a6adfec5925cb8
JVG <jeguglie@student.42.fr>
2020-02-16 19:32:59 +0100

    build

---
+++ client/build/precache-manifest.a94de952b2314bb9bf1f17fd6877aa95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cb9c9308b369835ad2045933833d5ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d43fef044a510adb5aaa",
+    "url": "/static/css/2.87d69661.chunk.css"
+  },
+  {
+    "revision": "450eb817c79fdbd60a6c",
+    "url": "/static/css/main.908f0748.chunk.css"
+  },
+  {
+    "revision": "d43fef044a510adb5aaa",
+    "url": "/static/js/2.8313309f.chunk.js"
+  },
+  {
+    "revision": "5b1cb998ee558fea3c9ab1508be776df",
+    "url": "/static/js/2.8313309f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "450eb817c79fdbd60a6c",
+    "url": "/static/js/main.619ee4ee.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
