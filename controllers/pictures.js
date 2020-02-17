1012459c8ecbdf12d007cf8036134dc7e514db3c
Jean-vincent GUGLIELMETTI <jeguglie@e1r4p1.42.fr>
2020-02-17 22:57:41 +0100

    update

--- controllers/pictures.js
+++ controllers/pictures.js
@@ -95,7 +95,7 @@ async function uploadPhoto(req, res) {
         let img_order = response.rows.length < 1 ? 0 : response.rows.length + 1;
         // Add image row and image order
         text = 'INSERT INTO pictures(user_id, img_link, img_order) VALUES ($1, $2, $3)';
-        values = [user_id, process.env.PORT ? 'https://matchappli.heroku.com/' : 'http://localhost:5000/api/' + pathImg, img_order];
+        values = [user_id, process.env.PORT ? 'https://matchappli.herokuapp.com/' : 'http://localhost:5000/api/' + pathImg, img_order];
         await pool.query(text, values);
         await updatetotalimage(user_id);
         await reorganizeImg(user_id);
