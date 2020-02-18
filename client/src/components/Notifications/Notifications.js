7f379a011b7bedac7d3a48c02e40d2e0f7cf67d4
Jean-vincent GUGLIELMETTI <jeguglie@e1r13p8.42.fr>
2020-02-18 20:48:36 +0100

    up

--- client/src/components/Notifications/Notifications.js
+++ client/src/components/Notifications/Notifications.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactNotification from 'react-notifications-component';
 import { store } from 'react-notifications-component';
 import io from 'socket.io-client';
-const url = process.env.NODE_ENV === "production" ? 'https://matchappli.herokuapp.com:8000' : 'http://localhost:8000/';
+const url = process.env.NODE_ENV === "production" ? 'https://matchappli.herokuapp.com:8000' : 'http://localhost:8000';
 class Notifications extends React.Component {
 
     constructor(props){
--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
-require('./controllers/sockets');
 const path = require('path');
 const host = process.env.PORT ? '0.0.0.0' : 'localhost';
 const port = process.env.PORT || 5000;
 const router = require('./utils/router');
+const server = app.listen(process.env.PORT || 5000, host, () => console.log(`Server listening on port ${port}`));
+var io = require('socket.io')();
+require('./controllers/sockets');
+
 
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, './client/build/')));
+
 app.use('/api/public', express.static('public'));
 app.use('/api', router);
 
@@ -15,4 +19,3 @@ app.use('/api', router);
 app.get('*', function(request, response) {
     response.sendFile(path.resolve(__dirname, './client/build/', 'index.html'));
 });
-app.listen(process.env.PORT || 5000, host, () => console.log(`Server listening on port ${port}`));
