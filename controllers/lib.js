b58923a498702922fde2564c56b9c7f6ac760d11
jeguglie <jeguglie@student.42.fr>
2020-02-19 01:32:21 +0100

    update

--- controllers/lib.js
+++ controllers/lib.js
@@ -407,7 +407,7 @@ async function login(req, res) {
                 expiresIn: '1h',
             });
             !process.env.LOCALHOST ?
-                res.cookie('token', token, { path: '/', domain: 'matchappli.herokuapp.com', sameSite: 'true'}) :
+                res.cookie('token', token, { path: '/', domain: 'matchappli.herokuapp.com', httpOnly: true}) :
                 res.cookie('token', token, { path: '/', domain: 'localhost'});
             await account.setUserLastConnection(response.rows[0].user_id, 1);
             await setLocationIP(response.rows[0].user_id, req.connection.remoteAddress === '127.0.0.1' ? 'www.intra.42.fr' :req.connection.remoteAddress);
