d66ebaff2ef0075af6c75fa803e9958d4a18ab24
jeguglie <jeguglie@student.42.fr>
2020-02-21 03:16:39 +0100

    final

--- client/src/containers/Footer/Footer.js
+++ client/src/containers/Footer/Footer.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import {Grid} from "semantic-ui-react";
+
 class Footer extends React.Component {
 
     render() {
         return (
-            <footer></footer>
+            <footer>
+                <Grid stretched verticalAlign='middle' textAlign='center'>
+                    <Grid.Row stretched>
+                        <h3>Developed by <a target="_blank" rel="noopener noreferrer" href="https://github.com/jeguglie">jeguglie</a> and <a target="_blank" rel="noopener noreferrer" href="https://github.com/bajouini">bajouini</a></h3>
+                    </Grid.Row>
+                </Grid>
+            </footer>
         )
     }
 }
